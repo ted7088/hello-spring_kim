ed1a68550e560f1e746b4abbca7ede65a6be921f
ted7088 <ted3131@naver.com>
2023-07-05 16:40:14 +0900

    commit

---
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepositroy.java
@@ -0,0 +1,41 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepositroy implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(),member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); //널이더라도 감싸서 return 시킴
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+
+    }
+
+    @Override
+    public List<Member> findll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+}
---
+++ hello-spring/src/test/java/hello/hellospring/repository/MemoryMeberRepositoryTest.java
@@ -0,0 +1,62 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemoryMeberRepositoryTest {
+
+    MemoryMemberRepositroy repositroy = new MemoryMemberRepositroy();
+    @AfterEach
+    public void afterEach(){
+        repositroy.clearStore(); //객체가 다 지워지면서 새로 만들어줌 , store.clear();를 테스트 클래스 하는 곳에도 만들어 줘야함
+    }
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("spring");
+
+        repositroy.save(member);
+
+        Member result = repositroy.findById(member.getId()).get();
+        assertThat(member).isEqualTo(result); //테스트 검증 (assertThat)
+    }
+
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repositroy.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repositroy.save(member2);
+
+        Member result = repositroy.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll(){
+
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repositroy.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repositroy.save(member2);
+
+        List<Member> result = repositroy.findll();
+
+        assertThat(result.size()).isEqualTo(2);
+
+    }
+}
