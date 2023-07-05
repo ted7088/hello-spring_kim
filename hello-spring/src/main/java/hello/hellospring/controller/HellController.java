faec93fad6990071a352ace8524a2a9b5bef071e
ted7088 <ted3131@naver.com>
2023-07-05 14:16:38 +0900

    commit

--- hello-spring/src/main/java/hello/hellospring/controller/HellController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HellController.java
@@ -4,13 +4,26 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HellController {
     @GetMapping("hello")
     public String hello(Model model){
-        model.addAttribute("data","hello!!");
+        model.addAttribute("data","spring!!");
         return "hello";
+    }
 
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello"+name; //"hello spring"
     }
 }
