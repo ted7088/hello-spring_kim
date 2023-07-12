3b5be126732aa1e2f66a6b2238c801dcbc7cd9c9
ted7088 <ted3131@naver.com>
2023-07-12 18:09:50 +0900

    commit

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,7 +1,5 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
@@ -9,36 +7,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
-    private EntityManager em;
-
-    @Autowired
-    public SpringConfig(EntityManager em){
-        this.em = em;
-    }
-
+//    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em){
+//        this.em = em;
+//    }
+// jpa 방식
 //    private DataSource dataSource;
 //
 //    public SpringConfig(DataSource dataSource) {
 //        this.dataSource = dataSource;
 //    }
+    //예전 방식
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
+//    @Bean
+//    public MemberRepository memberRepository(){
 //        return new MemoryMemberRepositroy();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//
+//    }
 
 }
--- hello-spring/src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 public interface SpringDataJpaMemberRepository extends JpaRepository <Member, Long> , MemberRepository{
 
+    //select m from Member m where m.name= ?
     @Override
     Optional<Member> findByName(String name);
 }
